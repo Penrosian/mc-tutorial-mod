d910143496a8a843497f12ba3ab0fee61c520298
Penrosian <106357770+Penrosian@users.noreply.github.com>
2025-01-07 19:41:52 -0500

    Add Pink Garnet & Raw Pink Garnet

--- src/main/java/net/penrosian/tutorialmod/TutorialMod.java
+++ src/main/java/net/penrosian/tutorialmod/TutorialMod.java
@@ -2,6 +2,7 @@ package net.penrosian.tutorialmod;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.penrosian.tutorialmod.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -11,6 +12,6 @@ public class TutorialMod implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
-		// Commit Test
+		ModItems.registerModItems();
 	}
 }
